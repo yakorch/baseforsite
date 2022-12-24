f9f12cc41ee8fe58e5e7940e0a035af75e346568
yakorch <korchyar19@gmail.com>
2022-12-24 14:36:49 +0200

    make it better

--- src/main/java/withoutexample/noart/abuser/AbuserController.java
+++ src/main/java/withoutexample/noart/abuser/AbuserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "somepath")
+@RequestMapping(path = "some-path")
 public class AbuserController {
     private AbuserService abs;
 
